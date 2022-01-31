4338cded871531572a8e6b3729b7e113ca83dda5
roryc261 <rorycameron75@gmail.com>
2022-01-31 16:23:01 +0000

    Static files test

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// Basic Express Server Setup 
 var express = require('express');
 var app = express();
+
+app.use(express.static('public')); // Serves static files
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
