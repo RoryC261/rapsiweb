0252cb1eed38276e7240ccf0056e00dd5a384e8d
roryc261 <rorycameron75@gmail.com>
2022-03-04 16:27:59 +0000

    Basic MongoDB Setup

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-// Basic Express Server Setup 
+// Express Server Setup 
 var express = require('express');
 var app = express();
 
+// MongoDB Setup
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/test_database";
+
 app.use(express.static('public')); // Serves static files
 
-
-app.listen(8080, '0.0.0.0', function(error) {
-    if (error) {
-        console.log("Something went wrong: ", error);
-    } else {
-        console.log("Server is listening on port 8080");
-    }
-});
+// Server Connection
+var db;
+MongoClient.connect(url, function(err, database){
+    if(err) throw err;
+    db = databse;
+    app.listen(8080);
+    console.log("Server Connected");
+})
