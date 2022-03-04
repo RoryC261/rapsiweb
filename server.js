5da4598a6b1241b01997bdc8a47f395a35640cef
roryc261 <rorycameron75@gmail.com>
2022-03-04 15:58:33 +0000

    Restart

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var app = express();
 
 app.use(express.static('public')); // Serves static files
 
-app.get('/', function(req, res) {
-    res.sendFile("index.html");
-});
 
 app.listen(8080, '0.0.0.0', function(error) {
     if (error) {
